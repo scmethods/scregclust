b94f90238c5a22a9faf8e5413a1a2caa8a7b1f1a
Felix Held <felix.held@gmail.com>
2023-03-10 16:50:34 +0100

    upd: add plotting for hyperparameter selection

--- R/plotting.R
+++ R/plotting.R
@@ -124,3 +124,392 @@ plot_regulator_network <- function(fit,
     ncol = 1
   )
 }
+
+#' @export
+plot.scregclust <- function(fit, ...) {
+  r2_cluster_data <- do.call(rbind, lapply(fit$results, function(r) {
+    do.call(rbind, lapply(r$output, function(o) {
+      idx <- !is.na(o$r2_cluster)
+
+      data.frame(
+        penalization = r$penalization,
+        cluster = seq_along(o$r2_cluster)[idx],
+        value = o$r2_cluster[idx]
+      )
+    }))
+  }))
+  r2_cluster_data$penalization <- factor(
+    r2_cluster_data$penalization, levels = fit$penalization
+  )
+  r2_cluster_data$variable <- "r2-per-cluster"
+
+  importance_data <- do.call(rbind, lapply(fit$results, function(r) {
+    do.call(rbind, lapply(seq_along(r$output), function(j) {
+      o <- r$output[[j]]
+      do.call(rbind, lapply(seq_len(ncol(o$models)), function(i) {
+        idx <- !is.na(o$importance[, i])
+        if (sum(idx) == 0) {
+          return(NULL)
+        }
+
+        data.frame(
+          penalization = r$penalization,
+          cluster = i,
+          value = o$importance[idx, i]
+        )
+      }))
+    }))
+  }))
+  importance_data$penalization <- factor(
+    importance_data$penalization, levels = fit$penalization
+  )
+  importance_data$variable <- "importance"
+
+  rbind(r2_cluster_data, importance_data) |>
+    ggplot2::ggplot() +
+    ggplot2::facet_wrap(
+      variable ~ .,
+      nrow = 2,
+      scales = "free_y",
+      strip.position = "left",
+      labeller = ggplot2::label_bquote(
+        .(
+          if (variable == "importance") {
+            "Regulator Importance"
+          } else {
+            "Predictive" ~ R^2 ~ "per module"
+          }
+        )
+      ),
+    ) +
+    ggplot2::geom_boxplot(
+      ggplot2::aes(x = penalization, y = value),
+      outlier.size = 0.5,
+      lwd = 0.25,
+    ) +
+    ggplot2::labs(x = "Penalization", y = NULL) +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(
+      panel.grid = ggplot2::element_blank(),
+      axis.line = ggplot2::element_line(
+        arrow = grid::arrow(length = grid::unit(1, "mm")),
+      ),
+      strip.background = ggplot2::element_blank(),
+      strip.placement = "outside",
+      line = ggplot2::element_line(linewidth = 0.25),
+      plot.margin = ggplot2::margin(t = 2, unit = "mm"),
+    )
+}
+
+collect_silhouette_data <- function(list_of_fits) {
+  do.call(rbind, lapply(list_of_fits, function(fit) {
+    do.call(rbind, lapply(seq_along(fit$results), function(i) {
+      r <- fit$results[[i]]
+      do.call(rbind, lapply(seq_along(r$output), function(j) {
+        o <- r$output[[j]]
+        k <- o$cluster[!r$is_regulator]
+
+        order_list <- lapply(seq_len(r$n_cl), function(cl) {
+          order(o$silhouette[k == cl])
+        })
+        gene <- do.call(c, lapply(seq_len(r$n_cl), function(cl) {
+          seq_along(k)[k == cl][order_list[[cl]]]
+        }))
+
+        data.frame(
+          order = seq_len(sum(k != -1)),
+          gene = gene,
+          silhouette = o$silhouette[gene],
+          cluster = as.factor(k[gene]),
+          n_cl = r$n_cl,
+          output = j,
+          penalization = r$penalization
+        )
+      }))
+    }))
+  }))
+}
+
+#' Plot individual silhouette scores
+#'
+#' @param list_of_fits A list of `scregclust` objects each fit to the same
+#'                     dataset across a variety of cluster counts.
+#' @param penalization Either a single numeric value requesting the results
+#'                     for the same penalty parameter across all fits in
+#'                     `list_of_fits`, or one for each individual fit.
+#' @param final_config The final configuration that should be visualised.
+#'                     Either a single number to be used for all fits in
+#'                     `list_of_fits`, or one for each individual fit.
+#'
+#' @return A [ggplot2] plot showing the the silhouette scores for each
+#'         supplied fit.
+#' @export
+plot_silhouettes <- function(list_of_fits, penalization, final_config = 1L) {
+  if (!(
+    is.numeric(penalization)
+    && (
+      (
+        length(penalization) == 1L
+        && all(sapply(list_of_fits, function(fit) {
+          penalization %in% fit$penalization
+        }))
+      ) || (
+        length(penalization) == length(list_of_fits)
+        && all(mapply(function(fit, p) {
+          p %in% fit$penalization
+        }, list_of_fits, penalization))
+      )
+    )
+  )) {
+    cli::cli_abort(c(
+      "{.var penalization} is not supplied correctly.",
+      "x" = "It needs to be one of the following two:",
+      "*" = "A single penalization parameter used in all fits.",
+      "*" = (
+        "A list of penalization parameters, exactly one for each supplied fit."
+      )
+    ))
+  }
+
+  #### TODO: Checking the correctness of this is a bit of a pain
+  ####       Do soon-ish!
+  # if (!(
+  #   is.numeric(final_config)
+  #   && all(as.integer(final_config) == final_config)
+  #   && (
+  #     (
+  #       length(final_config) == 1L
+  #       && all(sapply(list_of_fits, function(fit) {
+  #         final_config %in% fit$final_config
+  #       }))
+  #     ) || (
+  #       length(final_config) == length(list_of_fits)
+  #       && all(mapply(function(fit, p) {
+  #         p %in% fit$final_config
+  #       }, list_of_fits, final_config))
+  #     )
+  #   )
+  # )) {
+  #   cli::cli_abort(c(
+  #     "{.var final_config} is not supplied correctly.",
+  #     "x" = "It needs to be one of the following two:",
+  #     "*" = "A single final_config parameter used in all fits.",
+  #     "*" = (
+  #       "A list of final_config parameters, exactly one for each supplied fit."
+  #     )
+  #   ))
+  # }
+
+  silhouette_data <- collect_silhouette_data(list_of_fits)
+  cluster_counts <- sapply(list_of_fits, function(fit) fit$results[[1]]$n_cl)
+
+  silhouette_data$n_cl_lbl <- as.factor(sprintf("K = %d", silhouette_data$n_cl))
+
+  if (length(penalization) == 1L) {
+    silhouette_data <- silhouette_data[
+      silhouette_data$penalization == penalization,
+    ]
+  } else {
+    silhouette_data <- do.call(rbind, lapply(
+      seq_along(cluster_counts),
+      function(i) {
+        df <- silhouette_data[silhouette_data$n_cl == cluster_counts[i], ]
+        df[df$penalization == penalization[i]]
+      }
+    ))
+  }
+
+  if (length(final_config) == 1L) {
+    silhouette_data <- silhouette_data[
+      silhouette_data$output == final_config,
+    ]
+  } else {
+    silhouette_data <- do.call(rbind, lapply(
+      seq_along(cluster_counts),
+      function(i) {
+        df <- silhouette_data[silhouette_data$n_cl == cluster_counts[i], ]
+        df[df$output == final_config[i]]
+      }
+    ))
+  }
+
+  cluster_centers <- do.call(rbind, lapply(cluster_counts, function(n_cl) {
+    df <- silhouette_data[silhouette_data$n_cl == n_cl, ]
+    contained_clusters <- unique(df$cluster)
+
+    data.frame(
+      n_cl = n_cl,
+      cluster = contained_clusters,
+      order =  sapply(contained_clusters, function(cl) {
+        mean(df[df$cluster == cl, ]$order)
+      })
+    )
+  }))
+
+  avg_silhouette <- data.frame(
+    n_cl = cluster_counts,
+    silhouette = sapply(cluster_counts, function(n_cl) {
+      df <- silhouette_data[silhouette_data$n_cl == n_cl, ]
+      mean(df$silhouette)
+    })
+  )
+
+  silhouette_data |>
+    ggplot2::ggplot() +
+    ggplot2::facet_wrap(n_cl_lbl ~ .) +
+    ggplot2::geom_bar(
+      ggplot2::aes(x = order, y = silhouette, fill = cluster),
+      stat = "identity",
+    ) +
+    ggplot2::geom_text(
+      aes(x = order, y = -0.1, label = cluster),
+      data = cluster_centers,
+    ) +
+    ggplot2::geom_hline(
+      aes(yintercept = silhouette),
+      data = avg_silhouette,
+      linetype = "dashed",
+      color = "red",
+      linewidth = 0.25,
+    ) +
+    ggplot2::coord_flip() +
+    ggplot2::scale_fill_discrete(guide = "none") +
+    ggplot2::labs(x = "Module", y = "Silhouette score") +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(
+      panel.grid = ggplot2::element_blank(),
+      axis.text.y = ggplot2::element_blank(),
+    )
+}
+
+#' Plot average silhouette scores and average predictive \eqn{R^2}
+#'
+#' @param list_of_fits A list of `scregclust` objects each fit to the same
+#'                     dataset across a variety of cluster counts.
+#' @param penalization Either a single numeric value requesting the results
+#'                     for the same penalty parameter across all fits in
+#'                     `list_of_fits`, or one for each individual fit.
+#'
+#' @return A [ggplot2] plot showing the average silhouette score and the
+#'         average predictive \eqn{R^2}
+#'
+#' @export
+plot_cluster_count_helper <- function(list_of_fits, penalization) {
+  if (!(
+    is.numeric(penalization)
+    && (
+      (
+        length(penalization) == 1L
+        && all(sapply(list_of_fits, function(fit) {
+          penalization %in% fit$penalization
+        }))
+      ) || (
+        length(penalization) == length(list_of_fits)
+        && all(mapply(function(fit, p) {
+          p %in% fit$penalization
+        }, list_of_fits, penalization))
+      )
+    )
+  )) {
+    cli::cli_abort(c(
+      "{.var penalization} is not supplied correctly.",
+      "x" = "It needs to be one of the following two:",
+      "*" = "A single penalization parameter used in all fits.",
+      "*" = (
+        "A list of penalization parameters, exactly one for each supplied fit."
+      )
+    ))
+  }
+
+  silhouette_data <- collect_silhouette_data(list_of_fits)
+
+  avg_r2_cluster_data <- do.call(rbind, lapply(list_of_fits, function(fit) {
+    do.call(rbind, lapply(seq_along(fit$results), function(i) {
+      r <- fit$results[[i]]
+      r2_cluster <- do.call(c, lapply(seq_along(r$output), function(j) {
+        r$output[[j]]$r2_cluster
+      })) # average across different configurations
+
+      data.frame(
+        n_cl = r$n_cl,
+        penalization = r$penalization,
+        value = mean(r2_cluster),
+        variable = "avg-r2-cluster"
+      )
+    }))
+  }))
+
+  cluster_counts <- sapply(list_of_fits, function(fit) fit$results[[1]]$n_cl)
+
+  if (length(penalization) == 1) {
+    silhouette_data <- silhouette_data[
+      silhouette_data$penalization == penalization,
+    ]
+    avg_r2_cluster_data <- avg_r2_cluster_data[
+      avg_r2_cluster_data$penalization == penalization,
+    ]
+  } else {
+    silhouette_data <- do.call(rbind, lapply(
+      seq_along(cluster_counts),
+      function(i) {
+        df <- silhouette_data[silhouette_data$n_cl == cluster_counts[i], ]
+        df[df$penalization == penalization[i]]
+      }
+    ))
+    avg_r2_cluster_data <- do.call(rbind, lapply(
+      seq_along(cluster_counts),
+      function(i) {
+        df <- avg_r2_cluster_data[
+          avg_r2_cluster_data$n_cl == cluster_counts[i],
+        ]
+        df[df$penalization == penalization[i]]
+      }
+    ))
+  }
+
+  avg_silhouette <- sapply(seq_along(cluster_counts), function(i) {
+    df <- silhouette_data[silhouette_data$n_cl == cluster_counts[i], ]
+    mean(df$silhouette) # average across different configurations
+  })
+
+  rbind(
+    data.frame(
+      n_cl = cluster_counts,
+      penalization = penalization,
+      value = avg_silhouette,
+      variable = "avg-silhouette"
+    ),
+    avg_r2_cluster_data
+  ) |>
+    ggplot2::ggplot() +
+    ggplot2::facet_wrap(
+      variable ~ .,
+      nrow = 2,
+      scales = "free_y",
+      strip.position = "left",
+      labeller = ggplot2::label_bquote(
+        .(
+          if (variable == "avg-silhouette") {
+            "Average silhouette score"
+          } else {
+            "Avg. pred." ~ R^2 ~ "per module"
+          }
+        )
+      ),
+    ) +
+    ggplot2::geom_line(ggplot2::aes(n_cl, avg_silhouette), linewidth = 0.25) +
+    ggplot2::geom_point(ggplot2::aes(n_cl, avg_silhouette), size = 0.5) +
+    ggplot2::labs(x = "# of modules (K)", y = NULL) +
+    ggplot2::scale_x_continuous(breaks = cluster_counts) +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(
+      panel.grid = ggplot2::element_blank(),
+      axis.line = ggplot2::element_line(
+        arrow = grid::arrow(length = grid::unit(1, "mm")),
+      ),
+      strip.background = ggplot2::element_blank(),
+      strip.placement = "outside",
+      line = ggplot2::element_line(linewidth = 0.25),
+      plot.margin = ggplot2::margin(t = 2, unit = "mm"),
+    )
+}
--- R/scregclust-package.R
+++ R/scregclust-package.R
@@ -5,6 +5,7 @@
 #' @author Ida Larsson, Felix Held, Sven Nelander
 #' @import Rcpp
 #' @import cli
+#' @import ggplot2
 #' @importFrom prettyunits pretty_dt
 #' @importFrom Matrix Matrix sparseMatrix Diagonal t rowSums colSums summary
 #' @importFrom stats cor coef predict na.omit kmeans quantile sd dist setNames
@@ -13,6 +14,7 @@
 #' @importFrom methods is as
 #' @importFrom reshape melt
 #' @importFrom igraph graph_from_data_frame delete_edges delete_vertices layout_with_fr V E degree
+#' @importFrom grid arrow unit
 #' @useDynLib scregclust, .registration = TRUE
 "_PACKAGE"
 
